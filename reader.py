cce1d2d2d5e5030ed6402bd9ee7ff48814c2126a
maurits <maurits@TheRitzs-Air.local>
2018-09-04 12:39:20 +0200

    first 21cm commit

---
+++ reader.py
@@ -0,0 +1,47 @@
+# import modules
+import numpy as pd
+import pandas as pd
+import glob
+import matplotlib.pyplot as plt
+
+# set some global variables and lists etc.
+temp = []
+target = pd.DataFrame()
+data_folder = '..//21cm//data//*'
+# rip the galactic longitude from the 2nd line to use it later on
+# to improve I should really use a regex and rip digits after colon...
+for f in glob.glob(data_folder):
+    with open(f) as file:
+        line = file.readlines()[1]
+        lon = line[-5:-1]
+        check = f + lon
+# load all the data files in the list into DataFrame
+for data in glob.glob(data_folder):
+    with open(data) as file:
+        line = file.readlines()[1]
+        lon = line[-5:-1]
+    temp = pd.read_csv(data, header=13, delim_whitespace=True)
+# add the column names
+    temp.columns=['frequency power [Hz]','spectral density [dB/Hz]']
+# add the longitude column in a not-so-nice way
+    temp['longitude'] = lon
+# set MultiIndex on longitude and frequency columns
+    temp = temp.set_index(['longitude'])
+    target = target.append(temp)
+# sorting on entire index, not sure what this does for frequency so check
+    target = target.sort_index()
+
+# determine which measurements are significant for further processing
+#....
+
+# calculate the doppler shift
+target['doppler'] = (target['frequency power [Hz]'] - 1.421402)/1.421402
+
+# write-out to tab-delimited csv
+target.to_csv('..//21cm//21cmout.csv', sep='\t')
+
+#print some stats during dev
+#print(target.shape)
+#print(target.columns)
+print(target.head())
+print(target.tail())
