8371604a208b9944912d0622fc9fb99139a82e37
Maurits Polak <maurits.polak@protonmail.ch>
2018-09-07 14:05:11 +0200

    removed the for-loop for real now :-)

--- reader.py
+++ reader.py
@@ -8,18 +8,14 @@ import matplotlib.pyplot as plt
 temp = []
 target = pd.DataFrame()
 data_folder = '..//21cm//data//*'
+# in the next step:
 # rip the galactic longitude from the 2nd line to use it later on
-# to improve I should really use a regex and rip digits after colon...
-for f in glob.glob(data_folder):
-    with open(f) as file:
-        line = file.readlines()[1]
-        lon = line[-4:-1]
-        check = f + lon
-# load all the data files in the list into DataFrame
+# maybe I'd better use a regex and rip digits after colon?
 for data in glob.glob(data_folder):
     with open(data) as file:
         line = file.readlines()[1]
         lon = line[-5:-1]
+#load all the data files in the list into temporary DataFrame
     temp = pd.read_csv(data, header=13, delim_whitespace=True)
 # add the column names
     temp.columns=['frequency power [Hz]','spectral density [dB/Hz]']
@@ -27,20 +23,19 @@ for data in glob.glob(data_folder):
     temp['longitude'] = lon
 # set MultiIndex on longitude and frequency columns
     temp = temp.set_index(['longitude'])
-# write-out every spectrum in png format
-# still to add: substract baseline, gaussian fit
+# write-out spectrum in png format
     temp.plot(x='frequency power [Hz]', y='spectral density [dB/Hz]', kind='scatter', s=2)
     plt.savefig('..//21cm//output//' + lon + '.png')
     plt.close()
-#append df temp to df target
+#append df temp to totals df target
     target = target.append(temp)
-# sorting on entire index, not sure what this does for frequency so check
+# sorting the totals dataframe on index (but do I need to, really?)
     target = target.sort_index()
 
-# calculate the doppler shift, don't know how yet exactly
-#target['doppler'] = (target['frequency power [Hz]'] - 1.421402)/1.421402
+# still to add: substract baseline, gaussian fit...
+# which I' going to do on the totals dataframe
 
-# write-out to tab-delimited csv
+# write-out totals dataframe to tab-delimited csv
 target.to_csv('..//21cm//output//21cmout.csv', sep='\t')
 
 # -------DEV STUFF-------
