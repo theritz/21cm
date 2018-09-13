38779d43bd75ff45f9fb967f333fd57efd59caa8
Maurits Polak <maurits.polak@protonmail.ch>
2018-09-13 13:03:28 +0200

    added max value & doppler calculation

--- reader.py
+++ reader.py
@@ -5,6 +5,7 @@ import glob
 import matplotlib.pyplot as plt
 
 # set some global variables and lists etc.
+mw = []
 temp = []
 target = pd.DataFrame()
 data_folder = '..//21cm//data//*'
@@ -20,26 +21,30 @@ for data in glob.glob(data_folder):
     temp.columns=['frequency power [Hz]','spectral density [dB/Hz]']
 # add the longitude column in a not-so-nice way
     temp['longitude'] = lon
-# substract baseline using polynomial fit
-# ...
-# calculate velocity of source (but this is WRONG right now!)
-    temp['velocity'] = 299792458*((temp['frequency power [Hz]'] - 1420.45)/1420.45)
-# write-out spectrum in png format, disabled by default
-#    temp.plot(x='frequency power [Hz]', y='spectral density [dB/Hz]', kind='scatter', s=2)
+# substract baseline using polynomial or gaussian fit
+# ... 
+# print spectrum in png format, disabled by default
+#    plt.xlabel('Doppler shift [??]')
+#    plt.ylabel('spectral density [dB/Hz]')
+#    temp.plot(x='doppler', y='spectral density [dB/Hz]', kind='scatter', s=2)
 #    plt.savefig('..//21cm//output//' + lon + '.png')
-#    plt.close()
+#   plt.close()
 #append df temp to totals df target
     target = target.append(temp)
 # sorting the totals dataframe on index (but do I need to, really?)
 target = target.set_index(['longitude'])
-# set MultiIndex on longitude and frequency columns
-#target = target.sort_index()
+# get max values per longitude from df target and put in df mw
+mw = pd.DataFrame(target.max(level=0))
+# calculate doppler shift from df mw
+mw['doppler'] = 299792458*((mw['frequency power [Hz]'] - 1420.45)/1420.45)
+# sort df mw for cosmetic purposes
+mw = mw.sort_index()
 # write-out totals dataframe to tab-delimited csv
-#target.to_csv('..//21cm//output//21cmout.csv', sep='\t')
-
+#mw.to_csv('..//21cm//output//21cmout.csv', sep='\t')
 
 # -------DEV STUFF-------
-print(target.shape)
+#print(target.shape)
+print(mw.shape)
 #print(target)
 #print(target.info)
 #print(target.columns)
@@ -47,8 +52,6 @@ print(target.shape)
 #print(target.tail())
 #df.loc[row_indexer,column_indexer]
 #print(target.iloc[0:10, :])
-print(target.loc['016'])
-target.loc['016'].plot(x='frequency power [Hz]', y='spectral density [dB/Hz]', kind='scatter', s=2)
-target.loc['016'].plot(x='frequency power [Hz]', y='velocity', kind='scatter', s=2)
-plt.show()
-
+#print(target.loc['016'])
+#target.loc['016'].plot(x='frequency power [Hz]', y='spectral density [dB/Hz]', kind='scatter', s=2)
+#plt.show()
--- reader_backup.py
+++ reader_backup.py
@@ -1,5 +1,5 @@
 # import modules
-import numpy as pd
+import numpy as np
 import pandas as pd
 import glob
 import matplotlib.pyplot as plt
@@ -9,46 +9,47 @@ temp = []
 target = pd.DataFrame()
 data_folder = '..//21cm//data//*'
 # rip the galactic longitude from the 2nd line to use it later on
-# to improve I should really use a regex and rip digits after colon...
-for f in glob.glob(data_folder):
-    with open(f) as file:
-        line = file.readlines()[1]
-        lon = line[-4:-1]
-        check = f + lon
-# load all the data files in the list into DataFrame
+# maybe I'd better use a regex and rip digits after colon?
 for data in glob.glob(data_folder):
     with open(data) as file:
         line = file.readlines()[1]
-        lon = line[-5:-1]
+        lon = line[-4:-1]
+#load all the data files in the list into temporary DataFrame
     temp = pd.read_csv(data, header=13, delim_whitespace=True)
 # add the column names
     temp.columns=['frequency power [Hz]','spectral density [dB/Hz]']
 # add the longitude column in a not-so-nice way
     temp['longitude'] = lon
-# set MultiIndex on longitude and frequency columns
-    temp = temp.set_index(['longitude'])
-# write-out every spectrum in png format
-# still to add: substract baseline, gaussian fit
-    temp.plot(x='frequency power [Hz]', y='spectral density [dB/Hz]', kind='scatter', s=2)
-    plt.savefig('..//21cm//output//' + lon + '.png')
-#append df temp to df target
+# substract baseline using polynomial fit but I don't think this is needed
+# ...
+# calculate doppler shift
+    temp['doppler'] = 299792458*((temp['frequency power [Hz]'] - 1420.45)/1420.45)
+# write-out spectrum in png format, disabled by default
+#    plt.xlabel('Doppler shift [??]')
+#    plt.ylabel('spectral density [dB/Hz]')
+#    temp.plot(x='doppler', y='spectral density [dB/Hz]', kind='scatter', s=2)
+#    plt.savefig('..//21cm//output//' + lon + '.png')
+#   plt.close()
+#append df temp to totals df target
     target = target.append(temp)
-# sorting on entire index, not sure what this does for frequency so check
-    target = target.sort_index()
+# sorting the totals dataframe on index (but do I need to, really?)
+target = target.set_index(['longitude'])
+# set MultiIndex on longitude and frequency columns
+#target = target.sort_index()
+# write-out totals dataframe to tab-delimited csv
+#target.to_csv('..//21cm//output//21cmout.csv', sep='\t')
 
-# calculate the doppler shift, don't know how yet exactly
-#target['doppler'] = (target['frequency power [Hz]'] - 1.421402)/1.421402
-
-# write-out to tab-delimited csv
-target.to_csv('..//21cm//output//21cmout.csv', sep='\t')
 
 # -------DEV STUFF-------
-#print some stats during dev
-#print(target.shape)
+print(target.shape)
+#print(target)
 #print(target.info)
 #print(target.columns)
 print(target.head())
 print(target.tail())
 #df.loc[row_indexer,column_indexer]
 #print(target.iloc[0:10, :])
-#print(target.loc[' 016'])
+#print(target.loc['016'])
+#target.loc['016'].plot(x='frequency power [Hz]', y='spectral density [dB/Hz]', kind='scatter', s=2)
+#plt.show()
+
